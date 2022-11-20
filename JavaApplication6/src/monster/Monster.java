5384177b35186afcbd25b5534ab56431c9f22bd2
Zamboni <lorenzo.zamboni@buonarroti.tn.it>
2022-11-20 15:40:50 +0100

    Added methods to receive and deal damage, added endOfDuel method. Removed attack speed ratio attribute.

--- JavaApplication6/src/monster/Monster.java
+++ JavaApplication6/src/monster/Monster.java
@@ -1,8 +1,44 @@
 package monster;
 
 public class Monster {
+
     private String name;
     private Integer hp;
     private Integer dmg;
     private Double atkSpdRto; //Attack Speed Ratio
+
+    public Monster(String name, Integer hp, Integer dmg, Double atkSpdRto) {
+        this.name = name;
+        this.hp = hp;
+        this.dmg = dmg;
+    }
+
+    public void damageDealt(Integer enemyHP) {
+        if (enemyHP > dmg) {
+            enemyHP = -dmg;
+        } else {
+            endOfDuel('w');
+        }
+    }
+
+    public void damageReceveid(Integer enemyDMG) {
+        if (hp > enemyDMG) {
+            hp = -enemyDMG;
+        } else {
+            endOfDuel('l');
+        }
+
+    }
+
+    public String endOfDuel(char wol) {
+        String result = "";
+        switch (wol) {
+            case 'w':
+                result = "Congratulations, you won!";
+                break;
+            case 'l':
+                result = "End of duel, you lost!";
+        }
+        return result;
+    }
 }
